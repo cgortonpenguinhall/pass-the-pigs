debf1b7013f10b5d596e953957fcf8b29ebc0c5d
anash24 <anash24@penguinhall.org>
2022-11-28 11:09:40 -0500

    end of class 11/28

--- roll.js
+++ roll.js
@@ -1,4 +1,34 @@
-function roll(){
-    return 1
+// let results = [
+//     'no dot',
+//     'dot',
+//     'trotter',
+//     'razorback',
+//     'leaning jowler',
+//     'snouter'
+// ]
+// let allResults = results[Math.floor(Math.random() * results.length)]
+// console.log(allResults)
+
+function roll() {
+    return randomNumber
 }
-console.log(roll())
+
+
+let randomNumber = Math.random()
+
+if (randomNumber <= 0.349) {
+    console.log("Dot");
+} else if (randomNumber <= 0.651) {
+    console.log("No Dot")
+} else if (randomNumber <= 0.875) {
+    console.log("Razorback")
+} else if (randomNumber <= 0.963) {
+    console.log("Trotter")
+} else if (randomNumber <= 0.993) {
+    console.log("Snouter")
+} else if (randomNumber <= 1) {
+    console.log("Leabning Jowler")
+}
+
+
+
