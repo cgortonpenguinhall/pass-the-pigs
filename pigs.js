d0d95f839f2053317fef6f4ec6ac33f17b1d4457
erinwood24 <ewood24@penguinhall.org>
2022-11-30 09:08:06 -0500

    presentPigs

--- pigs.js
+++ pigs.js
@@ -5,7 +5,9 @@ function handleClick(whichBTN) {
     //makes  players turn  
     if (whichPlayer == 0 && (whichBTN == 'player0RollButton' || whichBTN == 'player0PassButton')){
         if (whichBTN == 'player0RollButton') {
-            roll()
+            document.getElementById('player0Pig1').innerHTML = roll()
+            document.getElementById('player0Pig2').innerHTML = roll()
+
         }else{
             console.log('thats a Pass  ' + whichPlayer)
             whichPlayer++
@@ -15,7 +17,8 @@ function handleClick(whichBTN) {
 
     if (whichPlayer == 1 && (whichBTN == 'player1RollButton' || whichBTN == 'player1PassButton')){
         if (whichBTN == 'player1RollButton') {
-            roll()
+            document.getElementById('player1Pig1').innerHTML = roll()
+            document.getElementById('player1Pig2').innerHTML = roll()
         }else{
             console.log('thats a Pass  ' + whichPlayer)
             whichPlayer++
@@ -26,7 +29,8 @@ function handleClick(whichBTN) {
 
     if (whichPlayer == 2 && (whichBTN == 'player2RollButton' || whichBTN == 'player2PassButton')){
         if (whichBTN == 'player2RollButton') {
-            roll()
+            document.getElementById('player2Pig1').innerHTML = roll()
+            document.getElementById('player2Pig2').innerHTML = roll()
         }else{
             console.log('thats a Pass  ' + whichPlayer)
             whichPlayer++
@@ -37,7 +41,8 @@ function handleClick(whichBTN) {
 
     if (whichPlayer == 3 && (whichBTN == 'player3RollButton' || whichBTN == 'player3PassButton')){
         if (whichBTN == 'player3RollButton') {
-            roll()
+            document.getElementById('player3Pig1').innerHTML = roll()
+            document.getElementById('player3Pig2').innerHTML = roll()
         }else{
             console.log('thats a Pass  ' + whichPlayer)
             whichPlayer = 0
