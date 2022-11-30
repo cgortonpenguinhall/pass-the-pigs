16871efcb178c5cde99c511665545e8ccf34ec92
anash24 <anash24@penguinhall.org>
2022-11-29 21:45:31 -0500

    homework

--- roll.js
+++ roll.js
@@ -1,34 +1,23 @@
-// let results = [
-//     'no dot',
-//     'dot',
-//     'trotter',
-//     'razorback',
-//     'leaning jowler',
-//     'snouter'
-// ]
-// let allResults = results[Math.floor(Math.random() * results.length)]
-// console.log(allResults)
 
 function roll() {
-    return randomNumber
+    let randomNumber = Math.random()
+    if (randomNumber <= 0.349) {
+        console.log("Dot")
+    } else if (randomNumber <= 0.651) {
+        console.log("No Dot")
+    } else if (randomNumber <= 0.875) {
+        console.log("Razorback")
+    } else if (randomNumber <= 0.963) {
+        console.log("Trotter")
+    } else if (randomNumber <= 0.993) {
+        console.log("Snouter")
+    } else if (randomNumber <= 1) {
+        console.log("Leaning Jowler")
+    }
 }
 
 
-let randomNumber = Math.random()
-
-if (randomNumber <= 0.349) {
-    console.log("Dot");
-} else if (randomNumber <= 0.651) {
-    console.log("No Dot")
-} else if (randomNumber <= 0.875) {
-    console.log("Razorback")
-} else if (randomNumber <= 0.963) {
-    console.log("Trotter")
-} else if (randomNumber <= 0.993) {
-    console.log("Snouter")
-} else if (randomNumber <= 1) {
-    console.log("Leabning Jowler")
+for (let i = 0; i <= 1000; i++) {
+    roll()
 }
-
-
 
