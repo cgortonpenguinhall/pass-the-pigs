4c128511e9a0be10c388e804e9589cd3dc6a8a7e
Eloucks24 <eloucks24@penguinhall.org>
2022-12-01 21:35:50 -0500

    homework

--- score.js
+++ score.js
@@ -8,19 +8,19 @@ function scorePigs(pig1, pig2) {
     } else if ((pig1 == "No Dot") && (pig2 == "No Dot")) {
         console.log("1")
     } else if ((pig1 == "Dot") && (pig2 == "No Dot")) {
-        console.log(0)
+        console.log("0")
     } else if ((pig1 == "No Dot") && (pig2 == "Dot")) {
-        console.log(0)
+        console.log("0")
     } else if ((pig1 == "Dot" || pig1 == "Not Dot") && (pig2 == "Snouter")) {
-        console.log(10)
+        console.log("10")
     } else if ((pig1 == "Snouter") && (pig2 == "Dot" ||pig2 == "No Dot")) {
-        console.log(10)
+        console.log("10")
     } else if ((pig1 == "Razer" ||pig1 ==  "Trotter" ||pig1 ==  "Leaning Jowler") && (pig2 == "Dot" || pig2 == "No Dot")) {
         console.log("5")
     } else if ((pig1 == "Razorback") && (pig2 == "Razorback")) {
-        console.log(20)
+        console.log("20")
     } else if ((pig1 == "Trotter") && (pig2 == "Trotter")) {
-        console.log()
+        console.log("20")
     }
 
 
@@ -31,6 +31,6 @@ function scorePigs(pig1, pig2) {
 
 }
 
-let pig1 = "razer"
-let pig2 = "Dot"
+let x = "Snouter"
+let y = "Dot"
 console.log(scorePigs(x, y))
