a125cd0312399369a77195ef34032364fccaee4f
cgortonpenguinhall <cgorton@penguinhall.org>
2022-12-02 14:02:58 -0500

    update score

--- score.js
+++ score.js
@@ -1,36 +1,32 @@
 function scorePigs(pig1, pig2) {
+let handScore = 0;
 
-
-    if ((pig1 == "Dot" || pig1 == "No Dot") && (pig2 == "Razer" ||pig2 ==  "Trotter" ||pig2 ==  "Leaning Jowler")) {
-        console.log("5")
-    } else if ((pig1 == "Dot") && (pig2 == "dot")) {
-        console.log("1")
-    } else if ((pig1 == "No Dot") && (pig2 == "No Dot")) {
-        console.log("1")
-    } else if ((pig1 == "Dot") && (pig2 == "No Dot")) {
-        console.log("0")
-    } else if ((pig1 == "No Dot") && (pig2 == "Dot")) {
-        console.log("0")
-    } else if ((pig1 == "Dot" || pig1 == "Not Dot") && (pig2 == "Snouter")) {
-        console.log("10")
-    } else if ((pig1 == "Snouter") && (pig2 == "Dot" ||pig2 == "No Dot")) {
-        console.log("10")
-    } else if ((pig1 == "Razer" ||pig1 ==  "Trotter" ||pig1 ==  "Leaning Jowler") && (pig2 == "Dot" || pig2 == "No Dot")) {
-        console.log("5")
-    } else if ((pig1 == "Razorback") && (pig2 == "Razorback")) {
-        console.log("20")
-    } else if ((pig1 == "Trotter") && (pig2 == "Trotter")) {
-        console.log("20")
+  // handle doubles
+  if (pig1 == pig2) {
+    if (pig1 == 'Dot' || pig1 == 'No dot') {
+      return 1;
+    } else if (pig1 == 'Razorback' || pig1 == 'Trotter') {
+      return 20;
+    } else if (pig1 == 'Snouter') {
+      return 40;
+    } else {
+      return 60;
     }
+  }
 
+  // handle not doubles
+  if (pig1 == 'Razorback' || pig2 == 'Razorback') {
+    handScore += 5;
+  }
+  if (pig1 == 'Trotter' || pig2 == 'Trotter') {
+    handScore += 5;
+  }
+  if (pig1 == 'Snouter' || pig2 == 'Snouter') {
+    handScore += 10;
+  }
+  if (pig1 == 'Leaning Jowler' || pig2 == 'Leaning Jowler') {
+    handScore += 15;
+  }
 
-    return 0
-
-
-
-
+  return handScore;
 }
-
-let x = "Snouter"
-let y = "Dot"
-console.log(scorePigs(x, y))
